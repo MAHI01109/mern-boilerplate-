b9de8cc1a30311c03e66bed20ab6e204239ce91d
MAHI01109 <mahendrasinghddnuk@gmail.com>
2024-07-25 11:06:08 +0530

    headr section degine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import About from './pages/About';
 import Profil from './pages/Profil';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    {/* headr  */}
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About/>} />
