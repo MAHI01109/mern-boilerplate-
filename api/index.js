7e3acc7cc6c4fc8b3197dcd6e23c497c50e36aff
MAHI01109 <mahendrasinghddnuk@gmail.com>
2024-07-25 14:18:06 +0530

    created user route and controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import connectDB from '../config/db.js'; // Adjusted import path
-
+import express from "express";
+import connectDB from "../config/db.js"; // Adjusted import path
+import userRoutes from "./routes/user.route.js";
 const app = express();
 
 // Middleware to handle JSON requests
@@ -10,5 +10,7 @@ app.use(express.json());
 connectDB();
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000!');
+  console.log("Server listening on port 3000!");
 });
+app.use('/api/user',userRoutes);
+
